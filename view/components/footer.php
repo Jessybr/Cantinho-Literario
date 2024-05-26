4c4c837f9102fbbac596901a68d6e85c20dda0cb
Jessybr <jessicabuenoramos5@gmail.com>
2024-05-26 19:24:33 -0300

    feat: create header and footer components

---
+++ view/components/footer.php
@@ -0,0 +1,3 @@
+<footer class="flex items-center ml-[19rem] h-14 bg-red-600 text-neutral-50">
+    <p class="ml-52">&copy;Copyright 2024 Jéssica B. Ramos. Todos os direitos reservados .</p>
+</footer>
---
+++ view/components/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clientes</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="../assets/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/table.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+<header class="fixed flex flex-col justify-start items-center h-screen w-[19rem] bg-red-600 text-neutral-50">
+    <div class="pt-5 w-[231px] h-1/6">
+        <h1 class="text-2xl">Cantinho Literário</h1><hr>
+    </div>
+
+    <div class="flex flex-col w-[231px] h-[520px] border-l border-l-[1] border-neutral-50">
+        <div class="pl-3 pb-2 border-b border-b-[1] border-neutral-50 font-black">
+            <h2>Menu</h2>
+        </div>
+        <div class="flex justify-between items-start flex-col w-4/5 h-3/4 pl-3 pt-1 font-light">
+            <div>
+                <ul>
+                    <li class="my-4"><a href="home.html">Início</a></li>
+                    <li class="my-4"><a href="livros.html">Livros</a></li>
+                    <li class="my-4"><a href="emprestimos.html">Empréstimos</a></li>
+                    <li class="my-4"><a href="clientes.html">Clientes</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="my-4">Funcionário:</li>
+                    <li class="my-4">Sair</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
