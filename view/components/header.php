bb477ea365ff4d43c468d15f72af1dbf69986885
Jessybr <jessicabuenoramos5@gmail.com>
2024-05-31 23:40:29 -0300

    feat: create responsive menu

--- view/components/header.php
+++ view/components/header.php
@@ -25,7 +25,38 @@ echo '<header>
                 </div>
             </div>
         </div>
+
+        
     </header>
+    <div class="menu-screen">
+        <span id="menu-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#fAfAfA" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>
+        </span>
+        <h1 class="text-2xl">Cantinho Literário</h1><hr>
+    </div>
+
+    <div class="menu-screen-side flex flex-col">
+            <div class="menu-title pl-3 pb-2">
+                <h2>Menu</h2>
+            </div>
+            <div class="menu-list flex justify-between items-start flex-col w-4/5 h-3/4 pl-3 pt-1 font-light">
+                <div>
+                    <ul>
+                        <li class="my-4"><a href="home.php">Início</a></li>
+                        <li class="my-4"><a href="livros.php">Livros</a></li>
+                        <li class="my-4"><a href="emprestimos.php">Empréstimos</a></li>
+                        <li class="my-4"><a href="clientes.php">Clientes</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li class="my-4">Funcionário:</li>
+                        <li class="my-4">Sair</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
 </body>';
 
 ?>
