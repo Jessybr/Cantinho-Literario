9fc0e97ba7b65611505d086349cb856e873eec86
Jessybr <jessicabuenoramos5@gmail.com>
2024-05-31 23:41:10 -0300

    refactor: edit screens theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", "./view/*.php"],
+  content: ["./**/*.{html,js}", "./view/*.php", "./.php"],
   theme: {
     fontFamily: {
         'sans': ['Jost', 'sans-serif'],
+      },
+    screens: {
+        'sm': '240px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '600px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '768px'
       },
     extend: {},
   },
