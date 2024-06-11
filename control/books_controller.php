a7a3a9f1d86da36b7b5ca31110491cd6af38443f
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-11 15:20:14 -0300

    feat: add class books and function to view all the datas in data base

--- control/books_controller.php
+++ control/books_controller.php
@@ -1,2 +1,9 @@
 <?php 
 require_once '../model/books.php';
+
+    class BooksControler extends Books{
+
+        public function viewAllDatas($inicio, $limite){
+            $books = new Books;
+            return  $datas = $books->getDatas($inicio, $limite);
+        }
