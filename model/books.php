dd14ea5422530af9228f28fff40e864c11dbd052
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-11 15:23:20 -0300

    feat: create getters and setters

--- model/books.php
+++ model/books.php
@@ -8,3 +8,59 @@
         private $editora;
         private $genero;
         private $quantidade;
+
+        public function getCodLivro(){
+            return $this->codLivro;
+        }
+
+        public function setCodLivro($codLivro){
+            return $this->codLivro = $codLivro;
+        }
+
+        public function getAtivo(){
+            return $this->ativo;
+        }
+
+        public function setAtivo($ativo){
+            return $this->ativo = $ativo;
+        }
+
+        public function getTitulo(){
+            return $this->titulo;
+        }
+
+        public function setTitulo($titulo){
+            return $this->titulo = $titulo;
+        }
+
+        public function getAutor(){
+            return $this->autor;
+        }
+
+        public function setAutor($autor){
+            return $this->autor = $autor;
+        }
+
+        public function getEditora(){
+            return $this->editora;
+        }
+
+        public function setEditora($editora){
+            return $this->editora = $editora;
+        }
+
+        public function getGenero(){
+            return $this->genero;
+        }
+
+        public function setGenero($genero){
+            return $this->genero = $genero;
+        }
+
+        public function getQuantidade(){
+            return $this->quantidade;
+        }
+
+        public function setQuantidade($quantidade){
+            return $this->quantidade = $quantidade;
+        }
