189083c0307c1049a50212e2cd9289b3c1cb27bb
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-11 15:22:49 -0300

    feat: create books class and their proprieties

---
+++ model/books.php
@@ -0,0 +1,10 @@
+<?php 
+    require_once "../control/connection.php";
+    class Books{
+        private $codLivro;
+        private $ativo;
+        private $titulo;
+        private $autor;
+        private $editora;
+        private $genero;
+        private $quantidade;
