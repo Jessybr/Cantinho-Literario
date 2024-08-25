6a0680664462166f488a58f53e7231fe707fc20f
Jessybr <jessicabuenoramos5@gmail.com>
2024-08-25 18:17:48 -0300

    feat: add function to update books

--- model/booksModel.php
+++ model/booksModel.php
@@ -39,17 +39,22 @@
         }
 
         public function getBook($Cod_livro){
-            //$datas = [];
-
             try{
                 $sql = "SELECT * FROM tb_livro WHERE Cod_livro = $Cod_livro";
                 $pdo = $this->conn->prepare($sql);
                 $pdo->execute();
-            
-                // while($result = $pdo->fetch(PDO::FETCH_ASSOC)){
-                //     $datas[] = $result;
-                // }
                 return $pdo;
+            }catch(PDOexception $e){
+                echo $e->getMessage();
+            }
+        }
+
+        public function updateBook($Cod_livro, $Foto, $Titulo, $Autor, $Editora, $Genero, $Quantidade){
+            try{
+                $sql = "UPDATE tb_livro SET foto = '$Foto', Titulo = '$Titulo', Autor = '$Autor', Editora = '$Editora', Genero = '$Genero', Quantidade = '$Quantidade' WHERE Cod_livro = $Cod_livro";
+                $pdo = $this->conn->prepare($sql);
+                $pdo->execute();
+                return TRUE;
             }catch(PDOexception $e){
                 echo $e->getMessage();
             }
